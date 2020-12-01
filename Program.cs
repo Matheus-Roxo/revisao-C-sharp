038062e105e60a235942f11760880324fd632c1b
Matheus Roxo Brandão <matheus.roxo.brandao31@gmail.com>
2020-12-01 20:09:44 -0300

    Revisão

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace revisao_C_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variavel*****************************
+            // string nome1 = "Matheus1";
+            // string nome2 = "Matheus2";
+            // string nome3 = "Matheus3";
+            // string nome4 = "Matheus4";
+            // string nome5 = "Matheus5"; //Só pode armazenar uma informação
+
+                              
+
+            //Array*********************************
+            //string[] nomes = {"Matheus0","Matheus1","Matheus2","Matheus3","Matheus4"}; //Aceita varias variaveis
+            //                      0       1       2       3       4
+
+            // string[] nomes = new string[10];
+            // int[] idade = new int[10];
+            // string[] rg = new string[10];
+            // string[] cpf = new string[10];
+
+            
+            // nomes[0] = "Matheus0";
+            // nomes[1] = "Matheus1";
+            // nomes[2] = "Matheus2";
+
+            //for*********************************
+            // for(int i = 0; i < nomes.Length; i++){
+            //     nomes[i] = "teste";
+            // }
+
+            //     Console.WriteLine(nomes[2]);
+            //     Console.ReadLine();
+
+            // string[] nomes = {"Matheus0","Matheus1","Matheus2","Matheus3","Matheus4"};
+
+            // for(var i = 0; i < 5; i++){
+            //     Console.WriteLine(nomes[i]);
+            // }//sempre vai ser um laço com um contador dentro
+
+            // int idade = 22;
+            // float altura = 1.78f;
+
+            //While e doWhile*********************************
+            // while(idade < 22 && altura < 1.81){
+            //     Console.WriteLine("Você precisa ser mais velho ou mais alto. :(");
+            // }
+
+            //string senha;
+
+            // do {
+            //     Console.WriteLine("Digite sua senha");
+            //     senha = Console.ReadLine();
+            // }while(senha != "123");
+            //     Console.WriteLine("Logado com sucesso");
+
+            //Switch case*********************************
+            // Console.WriteLine("Digite a sigla do estado SP ou RJ");
+            
+            // string estado = Console.ReadLine();
+
+            // switch (estado){
+
+            //     case "SP": 
+            //         Console.WriteLine("São Paulo - ligue para 11-11111-1111"); 
+            //     break;
+
+            //     case "RJ": 
+            //         Console.WriteLine("Rio de Janeiro - ligue para 21-11111-1111"); 
+            //     break;
+
+            //     default:
+            //         Console.WriteLine("Não atendemos esse estado");
+            //     break;
+            // }
+
+            //foreach*********************************
+            // string[] nomes = {"Matheus0","Matheus1","Matheus2","Matheus3","Matheus4"};
+            // foreach(var item in nomes){
+            //     Console.WriteLine(item);
+            // }
+        }
+    }
+}
